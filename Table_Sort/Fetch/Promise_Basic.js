eec2f3626e7d71795eb8e29b158c46f60bba28d7
Yumeno198271 <61045103+Yumeno198271@users.noreply.github.com>
2020-04-17 10:45:03 +0800

    Add files via upload

---
+++ Table_Sort/Fetch/Promise_Basic.js
@@ -0,0 +1,12 @@
+
+const promise1 = new Promise(function (resolve, reject) {
+    setTimeout(function () { resolve('Suceess'); }, 300);
+});
+
+promise1.then(function (value) {
+    console.log(value);
+}, function (ex) {
+    console.log("失敗原因:" + ex);
+});
+
+console.log(promise1);
